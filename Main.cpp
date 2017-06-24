fca269ce7acb4dd7637692d2f8f3ef62446b1416
Daniel <Daniel.embu96@Unitec.edu>
2017-06-23 18:56:14 -0600

    quiero pasar

--- Main.cpp
+++ Main.cpp
@@ -336,7 +336,19 @@ int main()
 						}//fin de login administrador
 						if (listapersonas.at(i)->getTipoPersona()=="Jugador")//login jugador
 						{
-							
+							cout<<"A ENTRADO A EL JUEGO DE BLACK JACK"<<endl;
+
+							for (int j = 0; j < listaMesas.size(); ++j)
+							{
+									if (listapersonas.at(i)->getNombre()==listaMesas.at(j)->getJugador()->getNombre())
+								{
+									cout<<endl<<"BIENVENIDO "<<listapersonas.at(i)->getNombre()<<endl;
+									cout<<"el numero de su mesa es el : "<<listaMesas.at(j)->getNumero()<<endl;
+									cout<<"el tipo de su mesa es : "<<listaMesas.at(j)->getTipo()<<endl;
+									cout<<"el repartidor  de su mesa es : "<<listaMesas.at(j)->getRepartidor()->getNombre()<<endl;
+											
+								}
+							}
 						}//fin login jugador
 					}
 				}
